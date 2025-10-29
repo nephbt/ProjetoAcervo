97516dfb9dc04fb4be15110958f9e25f32cfb24f
Bruno <bteles400@gmail.com>
2025-10-29 10:28:59 -0300

    fix: Arruma erro da endpoint de testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,4 @@
+import uuid
 from app import app
 
 def test_homepage():
@@ -7,7 +8,7 @@ def test_homepage():
 
 def test_cadastro_livros():
     livros = app.test_client()
-    response = livros.post("/livros", json={
+    response = livros.post("/livros/", json={
         "titulo": "livroTeste",
         "autor": "AutorTeste",
         "genero": "GeneroTeste",
@@ -18,9 +19,10 @@ def test_cadastro_livros():
 
 def test_cadastro_usuario():
     client = app.test_client()
+    unique_email = f"teste_{uuid.uuid4().hex[:6]}@exemplo.com"
     response = client.post("/usuarios/cadastro", json={
         "nome": "Teste",
-        "email": "teste@exemplo.com",
+        "email": unique_email,
         "senha": "12345678",
         "data_nasc": "2000-01-01"
     })
